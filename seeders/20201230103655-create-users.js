c2e02f243e74c4136965de970f85937defa9cc38
Praveen <praveenreddy1798@gmail.com>
2020-12-30 19:37:18 +0530

    deployment code

--- seeders/20201230103655-create-users.js
+++ seeders/20201230103655-create-users.js
@@ -4,8 +4,6 @@ const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const password = await bcrypt.hash('123456', 6)
-    const password1 = await bcrypt.hash('1234567', 6)
-    const password2 = await bcrypt.hash('1234568', 6)
     const createdAt = new Date()
     const updatedAt = createdAt
 
@@ -26,7 +24,7 @@ module.exports = {
       {
         username: 'jane',
         email: 'jane@email.com',
-        password: password1,
+        password: password,
         imageUrl:
           'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2190&q=80',
         createdAt,
@@ -35,7 +33,7 @@ module.exports = {
       {
         username: 'boss',
         email: 'boss@email.com',
-        password: password2,
+        password: password,
         imageUrl:
           'https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2122&q=80',
         createdAt,
