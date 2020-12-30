afd41b14f5ad1932da7fb69a686ad4501fa526ec
praveenreddy1798 <62658404+praveenreddy1798@users.noreply.github.com>
2020-12-31 01:27:24 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter, Switch } from 'react-router-dom'
+
+import ApolloProvider from './ApolloProvider'
+
+import './App.scss'
+
+import Home from './pages/home/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+import { AuthProvider } from './context/auth'
+import { MessageProvider } from './context/messages'
+import DynamicRoute from './util/DynamicRoute'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider>
+      <AuthProvider>
+        <MessageProvider>
+          <BrowserRouter>
+            <Container className="pt-5">
+              <Switch>
+                <DynamicRoute exact path="/" component={Home} authenticated />
+                <DynamicRoute path="/register" component={Register} guest />
+                <DynamicRoute path="/login" component={Login} guest />
+              </Switch>
+            </Container>
+          </BrowserRouter>
+        </MessageProvider>
+      </AuthProvider>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
